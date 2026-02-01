ff23bbff9f33a62cd36492ccaab866f92b5097ea
Gnaneshwar <gnaneshwar718@gmail.com>
2026-02-01 13:28:02 +0530

    feat: added about page ui and functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
-import { Explore } from './pages/Explore';
+import { About } from './pages/About';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
