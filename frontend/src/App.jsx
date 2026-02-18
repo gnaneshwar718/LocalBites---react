4309578c41a416df5116f46012c44c257283c877
gnaneshwar <gnaneshwar718@gmail.com>
2026-02-18 17:52:33 +0530

    Culturepage (#3)
    * feat: add homepage UI and logic
    
    * feat: explore page ui and functionality
    
    * feat: added about page ui and functionality
    
    * feat: add culturepage UI and logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
-import { Auth } from './pages/Auth';
+import { Culture } from './pages/Culture';
+import { Restaurant } from './pages/Restaurant';
+import { DishDetails } from './pages/DishDetails';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
+        <Route path="/culture" element={<Culture />} />
+        <Route path="/dish/:id" element={<DishDetails />} />
+        <Route path="/restaurant/:id" element={<Restaurant />} />
       </Routes>
     </Router>
   );
